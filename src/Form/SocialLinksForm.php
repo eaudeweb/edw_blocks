d4acf253b5de463cf7d56bab92d29a0ddd9a4e3b
AlexTrandafir09 <117073722+AlexTrandafir09@users.noreply.github.com>
2026-03-13 11:42:32 +0200

    refs #35391 - add social media links footer block (#1)
    * refs #35391 - add social media links footer block
    
    * refs #35391 - change max length of urls
    
    * refs #35391 - move social media links form from Serices to User Interface

---
+++ src/Form/SocialLinksForm.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\edw_blocks\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class SocialLinksForm extends ConfigFormBase {
+
+  public function getFormId(): string {
+    return 'edw_socialmedialinks_settings_form';
+  }
+
+  protected function getEditableConfigNames(): array {
+    return ['edw_socialmedialinks.settings'];
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $config = $this->config('edw_socialmedialinks.settings');
+
+    $form['facebook'] = [
+      '#type' => 'url',
+      '#title' => $this->t('Facebook URL'),
+      '#default_value' => $config->get('facebook') ?: '',
+      '#maxlength' => 512,
+    ];
+
+    $form['youtube'] = [
+      '#type' => 'url',
+      '#title' => $this->t('YouTube URL'),
+      '#default_value' => $config->get('youtube') ?: '',
+      '#maxlength' => 512,
+    ];
+
+    $form['linkedin'] = [
+      '#type' => 'url',
+      '#title' => $this->t('LinkedIn URL'),
+      '#default_value' => $config->get('linkedin') ?: '',
+      '#maxlength' => 512,
+    ];
+
+    $form['x'] = [
+      '#type' => 'url',
+      '#title' => $this->t('X URL'),
+      '#default_value' => $config->get('x') ?: '',
+      '#maxlength' => 512,
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->configFactory()
+      ->getEditable('edw_socialmedialinks.settings')
+      ->set('facebook', $form_state->getValue('facebook'))
+      ->set('youtube', $form_state->getValue('youtube'))
+      ->set('linkedin', $form_state->getValue('linkedin'))
+      ->set('x', $form_state->getValue('x'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
